61beee946df923a728ac31fac3490bd0bb28e557
cioina <alexei_cioina@yahoo.com>
2026-09-02 10:59:39 -0700

    update all

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -5,8 +5,8 @@ Provides some cpp functions
 
 # import sys
 # <OutDir>$(SolutionDir)$(Platform)\$(Configuration)\</OutDir>
-# VS_OUTPUT_PATH = r"simple_cpp_python\x64\Release"
-# VS_OUTPUT_PATH = r"simple_cpp_python\x64\Debug"
+# VS_OUTPUT_PATH = r"mixed-cpp-python\x64\Release"
+# VS_OUTPUT_PATH = r"mixed-cpp-python\x64\Debug"
 # sys.path.append(VS_OUTPUT_PATH)
 import spam
 initialized = True
