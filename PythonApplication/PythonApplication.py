e29103428c249d2c08833a6a96eca4f7a931f7c6
cioina <alexei_cioina@yahoo.com>
2026-09-02 12:55:12 -0700

    update all

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -1,25 +1,50 @@
-"""
-Provides some cpp functions
-"""
-# pylint: disable=invalid-name
+#!/usr/bin/env -S uv run --no-dev
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# import sys
-# <OutDir>$(SolutionDir)$(Platform)\$(Configuration)\</OutDir>
-# VS_OUTPUT_PATH = r"mixed-cpp-python\x64\Release"
-# VS_OUTPUT_PATH = r"mixed-cpp-python\x64\Debug"
-# sys.path.append(VS_OUTPUT_PATH)
-import spam
-initialized = True
+import os
+import pathlib
+import platform
+import subprocess
+import sys
 
-def main():
-    print(spam)
-    print("Module path:", getattr(spam, '__file__', 'Unknown (Built-in/Namespace)'))
-    print("Available names:", dir(spam))
+ROOT = pathlib.Path(os.path.abspath(__file__)).parent
 
-    print(spam.add(1,2))
-    print(spam.inc(123))
-    print(spam.say_hello("Alex"))
 
-if __name__ == '__main__':
-    main()
+def run():
+    env = dict(os.environ)
+    env["PYTHONUNBUFFERED"] = "1"
+    python = sys.executable
+    system = platform.system()
 
+    if system == "Darwin" or system == "Linux":
+        args = [
+            python,
+            "build-main.py",
+            *sys.argv[1:],
+        ]
+        make_dir = ROOT / "cpython-unix"
+        os.chdir(make_dir)
+        return os.execve(python, args, env)
+    elif system == "Windows":
+        args = [
+            python,
+            "cpp_example.py",
+            *sys.argv[1:],
+        ]
+        cwd = str(ROOT / "cpython-windows")
+        # print(args)
+        # print(cwd)
+        # print(env)
+
+        return subprocess.run(args, cwd=cwd, env=env, check=True, bufsize=0)
+    else:
+        raise Exception(f"Unsupported host system: {system}")
+
+
+if __name__ == "__main__":
+    try:
+        run()
+    except subprocess.CalledProcessError as e:
+        sys.exit(e.returncode)
---
+++ PythonApplication/cpython-windows/cpp_example.py
@@ -0,0 +1,22 @@
+"""
+Provides some cpp functions
+"""
+# import sys
+# <OutDir>$(SolutionDir)$(Platform)\$(Configuration)\</OutDir>
+# VS_OUTPUT_PATH = r"mixed-cpp-python\x64\Release"
+# VS_OUTPUT_PATH = r"mixed-cpp-python\x64\Debug"
+# sys.path.append(VS_OUTPUT_PATH)
+import spam
+initialized = True
+
+def main():
+    print(spam)
+    print("Module path:", getattr(spam, '__file__', 'Unknown (Built-in/Namespace)'))
+    print("Available names:", dir(spam))
+
+    print(spam.add(1,2))
+    print(spam.inc(123))
+    print(spam.say_hello("Alex"))
+
+if __name__ == '__main__':
+    main()
